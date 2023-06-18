33c3073744c2526a29ce7cd9299f7c1f91ee7ed0
[Carlos de Souza] <[carlostsouza@hotmail.com]>
2023-06-18 18:55:54 -0300

    Setup

--- src/weather_api.py
+++ src/weather_api.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = "http://api.weatherapi.com/v1"
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
 api_key = os.getenv("API-KEY")
 
+# Obrigatory params
 params = {
-    "key": api_key
+    "q": "Belo Horizonte",
+    "key": api_key,
+    "lang": "pt",  # Optional
 }
 
 response = requests.get(BASE_URL, params=params)
