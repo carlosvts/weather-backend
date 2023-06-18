3e33176039278efbe8bb44770099416b1816546d
[Carlos de Souza] <[carlostsouza@hotmail.com]>
2023-06-18 19:37:13 -0300

    Creating methods that will make the right connections, also some HTTP error handling

--- src/weather_api.py
+++ src/weather_api.py
@@ -2,18 +2,46 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from requests import HTTPError, Response
 
 load_dotenv()
 
-BASE_URL = "http://api.weatherapi.com/v1/current.json"
-
 api_key = os.getenv("API-KEY")
 
-# Obrigatory params
-params = {
-    "q": "Belo Horizonte",
-    "key": api_key,
-    "lang": "pt",  # Optional
-}
+BASE_URL = "http://api.weatherapi.com/v1/"
 
-response = requests.get(BASE_URL, params=params)
+
+def set_endpoint(api_method: str):
+    return BASE_URL + api_method
+
+
+def http_issuccess(response: Response):
+    if response.status_code != 200:
+        raise HTTPError(
+            "Erro ao conectar-se com a API, status code:", response.status_code
+        )
+
+
+def get_current_weather(q: str, lang: str | None = None):
+    api_endpoint_url = set_endpoint("current.json")
+
+    _params = {
+        "q": q,
+        "key": api_key,
+        "lang": lang,
+    }
+    response = requests.get(api_endpoint_url, params=_params)
+    http_issuccess(response)
+
+
+def get_forecast(q: str, days: str, lang: str | None = None):
+    api_endpoint_url = set_endpoint("forecast.json")
+
+    _params = {
+        "q": q,
+        "key": api_key,
+        "days": days,
+        "lang": lang,  # Optional
+    }
+    response = requests.get(api_endpoint_url, params=_params)
+    http_issuccess(response)
