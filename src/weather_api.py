a48d471e785518034181d17b2caa53d265ed10d3
[Carlos de Souza] <[carlostsouza@hotmail.com]>
2023-06-18 19:44:09 -0300

    Implementing this methods to a WeatherAPI class

--- src/weather_api.py
+++ src/weather_api.py
@@ -8,40 +8,42 @@ load_dotenv()
 
 api_key = os.getenv("API-KEY")
 
-BASE_URL = "http://api.weatherapi.com/v1/"
 
+class WeatherAPI():
+    def __init__(self, api_key, base_url="http://api.weatherapi.com/v1/"):
+        self.api_key = api_key
+        self.BASE_URL = base_url
 
-def set_endpoint(api_method: str):
-    return BASE_URL + api_method
+    def set_endpoint(self, api_method: str):
+        return self.BASE_URL + api_method
 
+    def http_issuccess(self, response: Response):
+        if response.status_code != 200:
+            raise HTTPError(
+                "Erro ao conectar-se com a API, status code:",
+                response.status_code
+            )
 
-def http_issuccess(response: Response):
-    if response.status_code != 200:
-        raise HTTPError(
-            "Erro ao conectar-se com a API, status code:", response.status_code
-        )
+    def get_current_weather(self, q: str, lang: str | None = None):
+        api_endpoint_url = self.set_endpoint("current.json")
 
+        _params = {
+            "q": q,
+            "key": api_key,
+            "lang": lang,
+        }
+        response = requests.get(api_endpoint_url, params=_params)
+        self.http_issuccess(response)
 
-def get_current_weather(q: str, lang: str | None = None):
-    api_endpoint_url = set_endpoint("current.json")
+    def get_forecast(self, q: str, days: str, lang: str | None = None):
+        api_endpoint_url = self.set_endpoint("forecast.json")
 
-    _params = {
-        "q": q,
-        "key": api_key,
-        "lang": lang,
-    }
-    response = requests.get(api_endpoint_url, params=_params)
-    http_issuccess(response)
+        _params = {
+            "q": q,
+            "key": api_key,
+            "days": days,
+            "lang": lang,  # Optional
+        }
 
-
-def get_forecast(q: str, days: str, lang: str | None = None):
-    api_endpoint_url = set_endpoint("forecast.json")
-
-    _params = {
-        "q": q,
-        "key": api_key,
-        "days": days,
-        "lang": lang,  # Optional
-    }
-    response = requests.get(api_endpoint_url, params=_params)
-    http_issuccess(response)
+        response = requests.get(api_endpoint_url, params=_params)
+        self.http_issuccess(response)
