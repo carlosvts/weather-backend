bcec8e4f5c2dd318467ed5aef27c56aa4d2d4952
carlosvts <carlosvtsdev@gmail.com>
2023-06-27 17:27:29 -0300

    Removing some unecessary comments

--- src/main.py
+++ src/main.py
@@ -3,12 +3,6 @@ from utils import (current_weather_localization_input,
                    weather_response_options)
 from weather_api import WeatherAPI, api_key
 
-# o que voce gostaria de fazer
-# forecast ou current
-# uma func pra cada opção
-# essa func pede os dados e passa eles para a WeatherAPI, essa mesma func
-# retorna as informações com um texto f" formatadin
-
 user_option = user_input()
 location = current_weather_localization_input()
 
